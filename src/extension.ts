be0241f76a5fb9a34e86752191cbf1393a4c5a1a
Neriya Cohen <ee10293847@gmail.com>
2021-03-24 01:53:31 +0200

    Merged all diffs into one file for every file, .lh folder now structured as root folder

--- src/extension.ts
+++ src/extension.ts
@@ -5,93 +5,80 @@ import * as fs from 'fs';
 import * as Diff from 'diff';
 import * as path from 'path';
 
-const root_dir = (vscode.workspace.workspaceFolders?.length ? vscode.workspace.workspaceFolders[0].uri.fsPath : path.dirname(vscode.workspace.textDocuments[0].fileName)).split("\\").join("/");
-const lh_dir = `${root_dir}/.local`;
-const patch_dir = `${lh_dir}/patches`;
-const commit_dir = `${lh_dir}/commits`;
-const commit_prefix = "commit";
-const patch_prefix = "patch";
-
-let numCommits = 0;
-let numPatches = 0;
-
-const onSave = vscode.workspace.onWillSaveTextDocument(async (document) => {
-	if(document.document.fileName.startsWith(lh_dir) || document.document.fileName.startsWith(lh_dir.split('/').join("\\"))) {
-		return;
+const root_dir = unixPath(vscode.workspace.workspaceFolders?.length ? vscode.workspace.workspaceFolders[0].uri.fsPath : path.dirname(vscode.workspace.textDocuments[0].fileName));
+const lh_dir = `${root_dir}/.lh`;
 const lh_ignore_file = `${lh_dir}/.lhignore`;
 let lh_ignore: string[] = [];
+
+const onSave = vscode.workspace.onWillSaveTextDocument(createDiff);
+
+function createDiff(document: vscode.TextDocumentWillSaveEvent) {
+	const fullPath = unixPath(document.document.fileName);
+	const relativeFilePath = relativePath(fullPath);
+	if (fullPath === lh_ignore_file) {
+		loadIgnoreFile();
 	}
-	const baseFileName = path.basename(document.document.fileName);
-	const newData = document.document.getText();
-	let oldFileName;
-	numCommits = getFileCommits(baseFileName).length;
-	if (numCommits < 1) {
-		newCommit(baseFileName, newData);
 	if (isIgnored(relativeFilePath)) {
 		return;
 	}
-	const diskData = fs.readFileSync(document.document.fileName).toString();
-	if (diskData !== newData) {
-		numPatches = fs.readdirSync(patch_dir).filter((file) => {
-			return new RegExp(`${patch_prefix}-${baseFileName}-[0-9]+.diff`).test(file);
-		}).length;
-		if (numPatches > 0) {
-			oldFileName = `${patch_dir}/${patch_prefix}-${baseFileName}-${numPatches}.diff`;
+	const newData = document.document.getText();
+	let fileDiff = loadFileDiff(relativeFilePath);
+	if (fileDiff) {
+		if (fileDiff.commits.length < 1) {
+			newCommit(fileDiff, newData);
 		} else {
-			oldFileName = `${commit_dir}/${commit_prefix}-${baseFileName}-${numCommits}.diff`;
+			const diskData = fs.readFileSync(document.document.fileName).toString();
+			if (newData !== diskData || newData !== fileDiff.commits[fileDiff.activeCommit - 1]) {
+				const patch = Diff.createPatch('', diskData, newData !== diskData ? newData : fileDiff.commits[fileDiff.activeCommit - 1]);
+				newPatch(fileDiff, patch);
+			}
 		}
-		const newFileName = `${patch_prefix}-${baseFileName}-${numPatches + 1}.diff`;
-		const patch = Diff.createTwoFilesPatch(`${oldFileName}`, `${patch_dir}/${newFileName}`, diskData, newData);
-		newPatch(newFileName, patch);
+	} else {
+		fileDiff = {
+			activeCommit: 1,
+			activePatch: 0,
+			commits: [newData],
+			patches: []
+		};
+		createFile(diffPathOf(relativeFilePath));
 	}
-});
-
-function newPatch(fileName: string, data: string): void {
-	fs.writeFile(`${patch_dir}/${fileName}`, data, (err) => {
-		if (err) {
-			console.log(err);
-		}
-	});
+	saveFileDiff(relativeFilePath, fileDiff);
 }
 
-function newCommit(fileName: string, data: string): void {
-	fs.writeFile(`${commit_dir}/${commit_prefix}-${fileName}-${++numCommits}.diff`, data, (err) => {
-		if (err) {
-			console.log(err);
-		}
-	});
-	getFilePatches(fileName).forEach(file => {
-		fs.unlinkSync(`${patch_dir}/${file}`);
-	});
+function newPatch(fileDiff: diff, data: string): void {
+	if (fileDiff.activePatch < fileDiff.patches.length) {
+		fileDiff.patches = fileDiff.patches.slice(0, fileDiff.activePatch);
+	}
+	fileDiff.patches.push(data);
+	fileDiff.activePatch = fileDiff.patches.length;
 }
 
-function getFilePatches(fileName: string): string[] {
-	return fs.readdirSync(patch_dir).filter((file) => {
-		return new RegExp(`${patch_prefix}-${fileName}-[0-9]+.diff`).test(file);
-	})
+function newCommit(fileDiff: diff, data: string): void {
+	fileDiff.commits.push(data);
+	fileDiff.activeCommit = fileDiff.commits.length;
+	fileDiff.activePatch = 0;
+	fileDiff.patches = [];
 }
 
-function getFileCommits(fileName: string): string[] {
-	return fs.readdirSync(commit_dir).filter((file) => {
-		return new RegExp(`${commit_prefix}-${fileName}-[0-9]+.diff`).test(file);
-	})
+function diffPathOf(relativeFilePath: string): string {
+	return `${lh_dir}/${relativeFilePath}.json`;
 }
 
-function getLastPatch(fileName: string): string {
-	const patches = getFilePatches(fileName);
-	return patches[patches.length - 1];
+function loadFileDiff(relativeFilePath: string): diff | undefined {
+	const diffPath = diffPathOf(relativeFilePath);
+	return fs.existsSync(diffPath) ? JSON.parse(fs.readFileSync(diffPathOf(relativeFilePath), "utf-8")) : undefined;
+	// return JSON.parse(fs.readFileSync(`${root_dir}/${fileRelativePath}`, "utf-8"));
 }
 
-function getLastCommit(fileName: string): string {
-	const commits = getFileCommits(fileName);
-	return commits[commits.length - 1];
+function saveFileDiff(relativeFilePath: string, fileDiff: diff) {
+	const diffPath = diffPathOf(relativeFilePath);
+	fs.writeFileSync(diffPath, JSON.stringify(fileDiff, null, 4));
+}
+
 function loadIgnoreFile(): void {
 	lh_ignore = fs.readFileSync(lh_ignore_file).toString().split("\r\n").filter(Boolean);
 }
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
 function isIgnored(fileName: string): boolean {
 	const a = lh_ignore.filter(function (pattern) {
 		return new RegExp(pattern).test(fileName);
@@ -99,55 +86,42 @@ function isIgnored(fileName: string): boolean {
 	return a;
 }
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "local-history" is now active!');
+function unixPath(path: string): string {
+	return path.split("\\").join("/");
+}
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('local-history.restore', async () => {
-		// The code you place here will be executed every time your command is executed
+function relativePath(path: string): string {
+	return unixPath(path).replace(root_dir + "/", "");
+}
 
-		// Display a message box to the user
-		const fileName = path.basename(vscode.window.activeTextEditor!.document.fileName);
-		const filePatches = getFilePatches(fileName);
-		const lastPatchNumber = filePatches.length;
-		let patchId: any = await vscode.window.showInputBox();
-		patchId = parseInt(patchId);
-		if(!patchId || patchId > lastPatchNumber) {
-			patchId = lastPatchNumber;
+async function restorePatch() {
+	// Display a message box to the user
+	const relativeFilePath = relativePath(vscode.window.activeTextEditor!.document.fileName);
+	let patchId: any = await vscode.window.showInputBox();
+	patchId = parseInt(patchId);
+	restorePatchA(relativeFilePath, patchId);
+}
+
+function restorePatchA(relativeFilePath: string, patchId: number) {
+	const fileDiff = loadFileDiff(relativeFilePath);
+	if(fileDiff) {
+		if (!patchId || patchId > fileDiff.patches.length) {
+			patchId = fileDiff.patches.length;
 		}
-		const editor = vscode.window.activeTextEditor;
-		if (editor == undefined) {
-			return;
-		}
-		// const patchString = fs.readFileSync(`${patch_dir}/${patch_prefix}-${fileName}-${patchId}.diff`).toString();
-		// const patchString = fs.readFileSync(`${patch_dir}/${patch_prefix}-${fileName}-1.diff`).toString();
-		// fs.writeFileSync(editor.document.uri.fsPath, fs.readFileSync(`${commit_dir}/${getLastCommit(fileName)}`));
-		let temp = fs.readFileSync(`${commit_dir}/${getLastCommit(fileName)}`).toString();
-		for(let i = 1; i <= patchId; i++) {
-			const patchString = fs.readFileSync(`${patch_dir}/${patch_prefix}-${fileName}-${i}.diff`).toString();
+		let temp = fileDiff.commits[fileDiff.activeCommit - 1];
+		for (let i = 0; i < patchId; i++) {
+			const patchString = fileDiff.patches[i];
 			const uniDiff = Diff.parsePatch(patchString);
 			temp = Diff.applyPatch(temp, uniDiff[0]);
 		}
-		fs.writeFileSync(editor.document.fileName, temp);
-		// Diff.applyPatches(uniDiff, {
-		// 	loadFile: (i, cb) => {
-		// 		console.log(i);
-		// 		// const f = editor.document.fileName;
-		// 		cb(false, temp);
-		// 	},
-		// 	patched: (i, c, cb) => {
-		// 		temp = c;
-		// 		cb(false);
-		// 	},
-		// 	complete: (e) => { 
-		// 		console.log(temp);
-		// 	}
-		// });
-		
-	});
+		fs.writeFileSync(`${root_dir}/${relativeFilePath}`, temp);
+		fileDiff.activePatch = patchId;
+		saveFileDiff(relativeFilePath, fileDiff);
+	} else {
+		vscode.window.showErrorMessage(`Diff info not found on file ${relativeFilePath}`);
+	}
+}
+
 function init() {
 	if (fs.existsSync(lh_ignore_file)) {
 		return;
@@ -160,11 +134,34 @@ function init() {
 	}
 }
 
+function createFile(relativePath: string, data?: string | undefined) {
+	if (fs.existsSync(`${root_dir}/${relativePath}`)) {
+		return;
+	}
+	fs.mkdirSync(path.dirname(relativePath), { recursive: true });
+	fs.writeFileSync(relativePath, data);
+}
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	// console.log('Congratulations, your extension "local-history" is now active!');
 	init();
 	loadIgnoreFile();
+	let disposable = vscode.commands.registerCommand('local-history.restore', restorePatch);
 	context.subscriptions.push(disposable);
 	context.subscriptions.push(onSave);
 }
 
 // this method is called when your extension is deactivated
 export function deactivate() { }
+
+type diff = {
+	activeCommit: number,
+	activePatch: number,
+	commits: string[],
+	patches: string[]
+}
